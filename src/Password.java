c026c6f52770bcfb1643579af58746b0eb3ee8d8
TheDeniableToast <filipnorberg02@outlook.com>
2019-12-13 11:11:55 +0100

    namngenerator mk1 klar

--- src/Password.java
+++ src/Password.java
@@ -7,28 +7,122 @@ public class Password {
     //Variables
         String s = JOptionPane.showInputDialog(null, "Välj en bokstav");
 
-        if (s.equals("a")) {
+        if (s.equalsIgnoreCase("a")) {
             JOptionPane.showMessageDialog(null, "Topp 10 populäraste manliga namn på A är " + "Anders, Alexander, Andreas, Axel, Arne, Anton, Adam, Ali, Arvid, Albin");
         }
 
-        if (s.equals("b")) {
+        if (s.equalsIgnoreCase("b")) {
             JOptionPane.showMessageDialog(null, "Topp 10 populäraste manliga namn på B är " + "Bengt, Bo, Björn, Bertil, Börje, Bernt, Bror, Birger, Benjamin, Benny");
         }
 
-        if (s.equals("c")) {
+        if (s.equalsIgnoreCase("c")) {
             JOptionPane.showMessageDialog(null, "Topp 10 populäraste manliga namn på C är " + "Carl, Christer, Christian, Claes, Christoffer, Conny, Charlie, Casper, Charles, Curt");
         }
 
-        if (s.equals("d")) {
+        if (s.equalsIgnoreCase("d")) {
             JOptionPane.showMessageDialog(null, "Topp 10 populäraste manliga namn på D är " + "Daniel, David, Dan, Dennis, Dick, Douglas, Dag, Dante, Donald, Dragan");
         }
 
-        if (s.equals("e")) {
+        if (s.equalsIgnoreCase("e")) {
             JOptionPane.showMessageDialog(null, "Topp 10 populäraste manliga namn på E är " + "Erik, Emil, Elias, Emanuel, Edvin, Evert, Einar, Ernst, Elliot, Edvard");
         }
 
-        if (s.equals("f")) {
+        if (s.equalsIgnoreCase("f")) {
             JOptionPane.showMessageDialog(null, "Topp 10 populäraste manliga namn på F är " + "Fredrik, Filip, Folke, Felix, Frank, Frans, Fabian, Fred, Fritz, Ferdinand");
+        }
+
+        if (s.equalsIgnoreCase("g")) {
+            JOptionPane.showMessageDialog(null, "Topp 10 populäraste manliga namn på G är " + "Gabriel, Georg, George, Gerd, Gerhard, Germund, Gert, Gilbert, Glenn, Göran");
+        }
+
+        if (s.equalsIgnoreCase("h")) {
+            JOptionPane.showMessageDialog(null, "Topp 10 populäraste manliga namn på H är " + "Hadi, Håkan, Hakon, Halvar, Halvard, Hampus, Hannes, Hans, Harald, Harry");
+        }
+
+        if (s.equalsIgnoreCase("i")) {
+            JOptionPane.showMessageDialog(null, "Topp 10 populäraste manliga namn på I är " + "Ian, Ibrahim, Ilyas, Ingemar, Ingemund, Ingvar, Isaac, Isac, Isak, Ismail");
+        }
+
+        if (s.equalsIgnoreCase("j")) {
+            JOptionPane.showMessageDialog(null, "Topp 10 populäraste manliga namn på J är " + "Jacob, Jake, Jakob, James, Jamie, Jan, Jarl, Jasper, Jens, Jerry");
+        }
+
+        if (s.equalsIgnoreCase("k")) {
+            JOptionPane.showMessageDialog(null, "Topp 10 populäraste manliga namn på K är " + "Kaj, Kalle, Karim, Karl, Kaspar, Kasper, Kenneth, Kenny, Kent, Kevin");
+        }
+
+        if (s.equalsIgnoreCase("l")) {
+            JOptionPane.showMessageDialog(null, "Topp 10 populäraste manliga namn på L är " + "Laban, Lage, Lars, Leandro, Leif, Lennart, Lennox, Leo, Leon, Leonard");
+        }
+
+        if (s.equalsIgnoreCase("m")) {
+            JOptionPane.showMessageDialog(null, "Topp 10 populäraste manliga namn på M är " + "Magnus, Maksim, Malcolm, Malek, Malik, Malte, Manfred, Manne, Måns, Manuel");
+        }
+
+        if (s.equalsIgnoreCase("n")) {
+            JOptionPane.showMessageDialog(null, "Topp 10 populäraste manliga namn på N är " + "Natanael, Natanel, Nathan, Nathanael, Nathaniel, Nick, Nicklas, Niclas, Nicolai, Nicolas");
+        }
+
+        if (s.equalsIgnoreCase("o")) {
+            JOptionPane.showMessageDialog(null, "Topp 10 populäraste manliga namn på O är " + "Odin, Ola, Olaus, Olav, Oliver, Oliwer, Olle, Olof, Olov, Omar");
+        }
+
+        if (s.equalsIgnoreCase("p")) {
+            JOptionPane.showMessageDialog(null, "Topp 10 populäraste manliga namn på P är " + "Pål, Pär, Patric, Patrick, Patrik, Paul, Pelle, Per, Peter, Petter");
+        }
+
+        if (s.equalsIgnoreCase("q")) {
+            JOptionPane.showMessageDialog(null, "Kunde inte hitta några manliga namn som börjar på Q");
+        }
+
+        if (s.equalsIgnoreCase("r")) {
+            JOptionPane.showMessageDialog(null, "Topp 10 populäraste manliga namn på R är " + "Rafael, Ragnar, Ralf, Rami, Raoul, Rasmus, Reidar, Reine, Reinhold, Richard");
+        }
+
+        if (s.equalsIgnoreCase("s")) {
+            JOptionPane.showMessageDialog(null, "Topp 10 populäraste manliga namn på S är " + "Said, Sakarias, Salomon, Sam, Samir, Samuel, Sander, Scott, Sean, Sebastian");
+        }
+
+        if (s.equalsIgnoreCase("t")) {
+            JOptionPane.showMessageDialog(null, "Topp 10 populäraste manliga namn på T är " + "Tage, Ted, Teddy, Teo, Teodor, Theo, Theodor, Theodore, Thomas, Thor");
+        }
+
+        if (s.equalsIgnoreCase("u")) {
+            JOptionPane.showMessageDialog(null, "Topp 4 populäraste manliga namn på U är " + "Ulf, Ulrik, Uno, Urban");
+        }
+
+        if (s.equalsIgnoreCase("v")) {
+            JOptionPane.showMessageDialog(null, "Topp 10 populäraste manliga namn på V är " + "Valdemar, Valentin, Valfrid, Valle, Valter, Verner, Victor, Vidar, Vigor, Viking");
+        }
+
+        if (s.equalsIgnoreCase("w")) {
+            JOptionPane.showMessageDialog(null, "Topp 10 populäraste manliga namn på W är " + "Waldemar, Walter, Wictor, Wiktor, Wilhelm, Wille, William, Willy, Wilton");
+        }
+
+        if (s.equalsIgnoreCase("x")) {
+            JOptionPane.showMessageDialog(null, "Topp 2 populäraste manliga namn på X är " + "Xander, Xavier");
+        }
+
+        if (s.equalsIgnoreCase("y")) {
+            JOptionPane.showMessageDialog(null, "Der enda manliga namnet på Y är " + "Yngve");
+        }
+
+        if (s.equalsIgnoreCase("z")) {
+            JOptionPane.showMessageDialog(null, "Topp 2 populäraste manliga namn på Z är " + "Zacharias, Zander");
+        }
+
+        if (s.equalsIgnoreCase("å")) {
+            JOptionPane.showMessageDialog(null, "Det enda manliga namnet på Å är " + "Åke");
+        }
+
+        if (s.equalsIgnoreCase("ä")) {
+            JOptionPane.showMessageDialog(null, "Kunde inte hitta några manliga namn på Ä");
+        }
+
+        if (s.equalsIgnoreCase("ö")) {
+            JOptionPane.showMessageDialog(null, "Topp 2 populäraste manliga namn på Ö är " + "Örjan, Östen");
         }
     }
 }
+
+
